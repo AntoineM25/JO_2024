f17887bafaef95495a68d13da19555adc3362067
AntoineM25 <massonantoine.pub@gmail.com>
2024-09-07 18:55:40 +0200

    Création model utilisateur

--- jo_projet/jo_app/models.py
+++ jo_projet/jo_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+# Modèle utilisateur
+class Utilisateur(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    sexe = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50, unique=True)
+    mot_de_passe = models.CharField(max_length=50)
+    adresse = models.CharField(max_length=100)
+    date_de_naissance = models.DateField()
+    date_d_inscription = models.DateField(auto_now_add=True)
+    est_administrateur = models.BooleanField(default=False)
+    cle_securisee_1 = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+
+# Modèle ticket
+# Modèle paiement
+# Modèle génération_ticket
