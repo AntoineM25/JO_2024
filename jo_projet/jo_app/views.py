a9bab1c50586a6319d2f59253f98d5409daebe39
AntoineM25 <massonantoine.pub@gmail.com>
2024-09-10 14:40:48 +0200

    Création vue + url  inscription

--- jo_projet/jo_app/views.py
+++ jo_projet/jo_app/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UtilisateurForm
 
 # Définition de la vue 'home'
 def home(request):
     return render(request, 'home.html')
+
+# Définition de la vue 'inscription'
+def inscription(request):
+    if request.method == 'POST':
+        form = UtilisateurForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('home')  
+    else:
+        form = UtilisateurForm()  
+
