cec178c705720a2afa46ae95a6af1ee86aa13c25
AntoineM25 <massonantoine.pub@gmail.com>
2024-09-24 01:08:33 +0200

    Forcer le redéploiement sur Heroku

--- jo_projet/settings.py
+++ jo_projet/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os, environ, cloudinary
+import os, environ, cloudinary, cloudinary.uploader, cloudinary.api
 
 cloudinary.config(
     cloud_name=os.getenv('dugtndapo'),
